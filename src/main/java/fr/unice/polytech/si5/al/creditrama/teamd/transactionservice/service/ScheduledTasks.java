e8be28c87d1a0f310ac69bd4b6226067b581aee6
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2020-02-14 17:49:23 +0100

    Use SAGA commandGateway to reject transaction (#45)

--- src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/transactionservice/service/ScheduledTasks.java
+++ src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/transactionservice/service/ScheduledTasks.java
@@ -1,8 +1,10 @@
 package fr.unice.polytech.si5.al.creditrama.teamd.transactionservice.service;
 
+import fr.unice.polytech.si5.al.creditrama.teamd.transactionservice.commands.ReverseTransferCommand;
 import fr.unice.polytech.si5.al.creditrama.teamd.transactionservice.model.Transaction;
 import fr.unice.polytech.si5.al.creditrama.teamd.transactionservice.model.TransactionState;
 import fr.unice.polytech.si5.al.creditrama.teamd.transactionservice.repository.TransactionRepository;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ import java.util.List;
 @Component
 public class ScheduledTasks {
     private TransactionRepository transactionRepository;
+    private CommandGateway commandGateway;
 
     @Autowired
-    public ScheduledTasks(TransactionRepository transactionRepository) {
+    public ScheduledTasks(TransactionRepository transactionRepository, CommandGateway commandGateway) {
         this.transactionRepository = transactionRepository;
+        this.commandGateway = commandGateway;
     }
 
     @Scheduled(fixedDelay = 120000)
@@ -25,10 +29,11 @@ public class ScheduledTasks {
         transactions.stream()
                 .filter(transaction -> transaction.getCreatedTransaction().plusMinutes(2).isAfter(LocalDateTime.now()))
                 .forEach(transaction -> {
-                    transaction.setCode((short) 0);
-                    transaction.setTransactionState(TransactionState.CANCEL);
-                    transactionRepository.save(transaction);
-                    System.out.println(String.format("Cancel transaction %s", transaction.getUuid()));
+//                    transaction.setCode((short) 0);
+//                    transaction.setTransactionState(TransactionState.CANCEL);
+                    System.out.println(String.format("Canceling transaction %s", transaction.getUuid()));
+                    commandGateway.send(new ReverseTransferCommand(transaction.getUuid(), transaction.getSource().getIban(), transaction.getDest().getIban(), transaction.getAmount()));
+//                    transactionRepository.save(transaction);
                 });
     }
 }
